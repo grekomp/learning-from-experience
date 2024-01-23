5a48c68827b7588d6ed03d0bc053b143d6491fec
Grzegorz Palian <grzegorz.palian@sonalake.com>
2024-01-23 10:47:44 +0100

    Removed boilerplate code

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,15 +7,12 @@
 	<h1>About this app</h1>
 
 	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
+		This is a simple SvelteKit application that I'm using to experiment with different web
+		technologies, and documment my findings.
 	</p>
 
-	<pre>npm create svelte@latest</pre>
-
 	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
+		It will start out as an extremely bare-bones site, and I'll add more features as I check out
+		more technologies.
 	</p>
 </div>
