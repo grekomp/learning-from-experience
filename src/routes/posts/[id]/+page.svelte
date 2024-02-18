202d3058fe7303829453ab27ac5753c70fe879de
Grzegorz Palian <grzegorz.palian@sonalake.com>
2024-02-18 17:30:36 +0100

    Added a page for rendering single post content

---
+++ src/routes/posts/[id]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Markdown from '$lib/components/markdown/markdown.svelte';
+
+	export let data;
+</script>
+
+<header class="mx-auto my-14 flex max-w-5xl flex-col gap-5">
+	<h1 class="text-5xl font-black">{data.post.title}</h1>
+	<h2 class="max-w-lg text-xl text-muted-foreground">
+		{data.post.subtitle}
+	</h2>
+</header>
+
+<section class="mx-auto flex max-w-5xl flex-col gap-5">
+	<Markdown source={data.post.content} />
+</section>
