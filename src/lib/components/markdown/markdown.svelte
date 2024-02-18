4c2ae7b7624a8198f2caa626933859201dbb0452
Grzegorz Palian <grzegorz.palian@sonalake.com>
2024-02-18 17:29:27 +0100

    Added a custom renderer for markdown headings

---
+++ src/lib/components/markdown/markdown.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import HeadingRenderer from '$lib/components/markdown/renderers/heading-renderer.svelte';
+	import SvelteMarkdown from 'svelte-markdown';
+
+	export let source: string | null | undefined;
+</script>
+
+<SvelteMarkdown
+	{source}
+	renderers={{
+		heading: HeadingRenderer,
+	}}
+/>
