464c704ee05231d03992d574c0e4f2fc72f236c7
Grzegorz Palian <grzegorz.palian@sonalake.com>
2024-02-20 23:52:36 +0100

    Sorted posts by date

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Markdown from '$lib/components/markdown/markdown.svelte';
 	import { Button } from '$lib/components/ui/button';
 	import * as Card from '$lib/components/ui/card';
 
@@ -10,19 +9,32 @@
 	<p>{data.error.message}</p>
 {:else}
 	<section class="mx-auto flex max-w-5xl flex-col gap-5">
-		{#each data.posts as post}
-			<Card.Root>
-				<Card.Header>
-					<Card.Title>{post.title}</Card.Title>
-					<Card.Description>{post.subtitle}</Card.Description>
-				</Card.Header>
-				<Card.Content>
-					<Markdown source={post.content} />
-				</Card.Content>
-				<Card.Footer>
-					<Button href={`/posts/${post.id}`} variant="outline">Continue reading</Button>
-				</Card.Footer>
-			</Card.Root>
+		{#each data.posts.sort((a, b) => {
+			return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+		}) as post}
+			<article class="mx-3">
+				<Card.Root>
+					<Card.Header>
+						<Card.Title tag="h2">{post.title}</Card.Title>
+						<Card.Description>
+							Created on <time datetime={post.created_at}
+								>{new Date(post.created_at).toDateString()}</time
+							>
+						</Card.Description>
+					</Card.Header>
+					<Card.Content>
+						{#if post.subtitle}
+							<p class="text-muted-foreground">{post.subtitle}</p>
+						{/if}
+						{#if post.summary}
+							<p>{post.summary}</p>
+						{/if}
+					</Card.Content>
+					<Card.Footer>
+						<Button href={`/posts/${post.id}`} variant="outline">Continue reading</Button>
+					</Card.Footer>
+				</Card.Root>
+			</article>
 		{/each}
 	</section>
 {/if}
