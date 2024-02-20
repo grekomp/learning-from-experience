0b2c3fbd7614c399d7b618c8c4f9b37e3ae7b439
Grzegorz Palian <grzegorz.palian@sonalake.com>
2024-02-20 23:53:05 +0100

    Added horizontal margin to post content

--- src/routes/posts/[id]/+page.svelte
+++ src/routes/posts/[id]/+page.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
 	import Markdown from '$lib/components/markdown/markdown.svelte';
+	import H1 from '$lib/components/ui/basic-html/h1.svelte';
 
 	export let data;
 </script>
 
-<header class="mx-auto my-14 flex max-w-5xl flex-col gap-5">
-	<h1 class="text-5xl font-black">{data.post.title}</h1>
-	<h2 class="max-w-lg text-xl text-muted-foreground">
-		{data.post.subtitle}
-	</h2>
-</header>
+<div class="mx-4">
+	<header class="mx-auto my-14 flex max-w-5xl flex-col gap-5">
+		<H1>{data.post.title}</H1>
+		<h2 class="max-w-lg text-xl text-muted-foreground">
+			{data.post.subtitle}
+		</h2>
+	</header>
 
-<section class="mx-auto flex max-w-5xl flex-col gap-5">
-	<Markdown source={data.post.content} />
-</section>
+	<section class="mx-auto flex max-w-5xl flex-col gap-5">
+		<Markdown source={data.post.content} />
+	</section>
+</div>
