d933c91abf488cc3d27f6dcf96f75f45c32188c2
Grzegorz Palian <grzegorz.palian@sonalake.com>
2024-04-15 11:14:42 +0200

    Added a simple interaction stack debug component

---
+++ src/lib/modules/interaction-stack/interaction-stack-debug.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { InteractionStack } from './interaction-stack';
+
+	export let interactionStack: InteractionStack;
+</script>
+
+<div
+	class="pointer-events-none fixed bottom-2 right-2 z-[900] rounded border-2 border-neutral-900 bg-neutral-800 p-4 opacity-50"
+>
+	{#each $interactionStack.getAll() as interaction}
+		<div>
+			<div>{interaction.id}</div>
+			<div>{interaction.interactionType}</div>
+			<div>{interaction.status}</div>
+		</div>
+	{/each}
+</div>
