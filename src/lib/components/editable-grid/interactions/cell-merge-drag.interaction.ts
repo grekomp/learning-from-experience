0bb0f9534d1a5048e32119b56c0d1793bcd52f2e
Grzegorz Palian <grzegorz.palian@sonalake.com>
2024-04-18 23:56:53 +0200

    Implemented grid cell merge interaction

--- src/lib/components/editable-grid/interactions/cell-merge-drag.interaction.ts
+++ src/lib/components/editable-grid/interactions/cell-merge-drag.interaction.ts
@@ -1,8 +1,11 @@
+import {
+	EditableGridCellEvents,
+	isGridCellEventOfType,
+} from '$lib/components/editable-grid/dom-events/editable-grid-cell-events';
 import type { EditableGridController } from '$lib/components/editable-grid/editable-grid-controller';
 import type { EditableGridCellData } from '$lib/components/editable-grid/editable-grid.model';
 import { InteractionBase } from '$lib/modules/interaction-stack/interaction-base';
 import type { InteractionStack } from '$lib/modules/interaction-stack/interaction-stack';
-import { noop } from '$lib/utils/noop.utils';
 
 export interface EditableGridCellMergeDragInteractionData {
 	grid: EditableGridController;
@@ -20,23 +23,48 @@ export class EditableGridCellMergeDragInteraction extends InteractionBase<
 > {
 	constructor(stack: InteractionStack, data: EditableGridCellMergeDragInteractionData) {
 		super(stack, data, editableGridCellMergeDragInteractionType);
+
+		this.onCellMouseEnter = this.onCellMouseEnter.bind(this);
+		this.onCellClick = this.onCellClick.bind(this);
 	}
 
 	setTargetCell(cell: EditableGridCellData) {
 		if (cell === this.data.fromCell) {
-			this.data.toCell = undefined;
+			this.data = { ...this.data, toCell: undefined };
 			return;
 		}
 
-		this.data.toCell = cell;
+		this.data = { ...this.data, toCell: cell };
 	}
 
-	_onStart = noop;
+	_onStart() {
+		document.addEventListener(EditableGridCellEvents.MouseEnter, this.onCellMouseEnter);
+		document.addEventListener(EditableGridCellEvents.Click, this.onCellClick);
+	}
 	_onComplete() {
 		if (this.data.fromCell && this.data.toCell) {
 			this.data.grid.mergeCells(this.data.fromCell, this.data.toCell);
 		}
 	}
-	_onCancel = noop;
-	_onStackPop = noop;
+	_onDispose() {
+		document.removeEventListener(EditableGridCellEvents.MouseEnter, this.onCellMouseEnter);
+		document.removeEventListener(EditableGridCellEvents.Click, this.onCellClick);
+	}
+
+	onCellMouseEnter(event: Event) {
+		if (!isGridCellEventOfType(event, EditableGridCellEvents.MouseEnter)) return;
+
+		event.preventDefault();
+		event.stopPropagation();
+
+		this.setTargetCell(event.detail.cell);
+	}
+	onCellClick(event: Event) {
+		if (!isGridCellEventOfType(event, EditableGridCellEvents.Click)) return;
+
+		event.preventDefault();
+		event.stopPropagation();
+
+		this.complete();
+	}
 }
