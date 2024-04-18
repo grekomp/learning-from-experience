ac49848f605b15dd16480f4fc8ea245f3007c0de
Grzegorz Palian <grzegorz.palian@sonalake.com>
2024-04-18 23:38:38 +0200

    Added support for importing svg files as svelte components

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
+import svg from '@poppanator/sveltekit-svg';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		svg({
+			svgoOptions: {
+				plugins: [
+					{
+						name: 'preset-default',
+						params: {
+							overrides: {
+								removeViewBox: false,
+							},
+						},
+					},
+				],
+			},
+		}),
+	],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+	},
 });
