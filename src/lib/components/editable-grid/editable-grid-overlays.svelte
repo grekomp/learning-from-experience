6e526560193076751c5f6a33682090820fd4992a
Grzegorz Palian <grzegorz.palian@sonalake.com>
2024-04-28 22:03:23 +0200

    Applied a workaround for a bug where the subgrid lines wouldn't get reordered when parent grid lines are reordered

--- src/lib/components/editable-grid/editable-grid-overlays.svelte
+++ src/lib/components/editable-grid/editable-grid-overlays.svelte
@@ -1,19 +1,10 @@
 <script lang="ts">
 	import type { EditableGridOverlayData } from '$lib/components/editable-grid/editable-grid-overlay.model';
-	import { gridEndLine, gridStartLine } from '$lib/components/editable-grid/editable-grid.model';
 	import OverlayRenderer from '$lib/components/editable-grid/overlay-renderers/overlay-renderer.svelte';
 
 	export let overlays: EditableGridOverlayData[];
 </script>
 
-<div
-	class="pointer-events-none z-[900] grid grid-cols-subgrid grid-rows-subgrid"
-	style:grid-row-start={gridStartLine}
-	style:grid-row-end={gridEndLine}
-	style:grid-column-start={gridStartLine}
-	style:grid-column-end={gridEndLine}
->
-	{#each overlays as overlay}
-		<OverlayRenderer {overlay} />
-	{/each}
-</div>
+{#each overlays as overlay}
+	<OverlayRenderer {overlay} />
+{/each}
--- src/lib/components/editable-grid/overlay-renderers/overlay-renderer.svelte
+++ src/lib/components/editable-grid/overlay-renderers/overlay-renderer.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
+	import { EditableGridController } from '$lib/components/editable-grid/editable-grid-controller';
 	import {
 		OverlayTargetType,
 		type EditableGridOverlayData,
 	} from '$lib/components/editable-grid/editable-grid-overlay.model';
+	import { gridContext } from '$lib/components/editable-grid/editable-grid.model';
 	import AreasOverlayRenderer from '$lib/components/editable-grid/overlay-renderers/areas-overlay-renderer.svelte';
 	import CellsOverlayRenderer from '$lib/components/editable-grid/overlay-renderers/cells-overlay-renderer.svelte';
 	import ContainerOverlayRenderer from '$lib/components/editable-grid/overlay-renderers/container-overlay-renderer.svelte';
 	import LinesOverlayRendrerer from '$lib/components/editable-grid/overlay-renderers/lines-overlay-renderer.svelte';
+	import { getContext } from 'svelte';
+
+	const grid = getContext<EditableGridController>(gridContext);
 
 	export let overlay: EditableGridOverlayData;
 	let overlayWithDefaults: EditableGridOverlayData;
@@ -17,18 +22,20 @@
 	};
 </script>
 
-{#if overlayWithDefaults.targetType === OverlayTargetType.Cells}
-	<CellsOverlayRenderer overlay={overlayWithDefaults} />
-{/if}
-{#if overlayWithDefaults.targetType === OverlayTargetType.Lines}
-	<LinesOverlayRendrerer overlay={overlayWithDefaults} />
-{/if}
-{#if overlayWithDefaults.targetType === OverlayTargetType.Areas}
-	<AreasOverlayRenderer overlay={overlayWithDefaults} />
-{/if}
-{#if overlayWithDefaults.targetType === OverlayTargetType.Container}
-	<ContainerOverlayRenderer overlay={overlayWithDefaults} />
-{/if}
-{#if overlayWithDefaults.targetType === OverlayTargetType.Custom}
-	<svelte:component this={overlay.component} overlay={overlayWithDefaults} />
-{/if}
+<div class="contents">
+	{#if overlayWithDefaults.targetType === OverlayTargetType.Cells}
+		<CellsOverlayRenderer overlay={overlayWithDefaults} />
+	{/if}
+	{#if overlayWithDefaults.targetType === OverlayTargetType.Lines}
+		<LinesOverlayRendrerer overlay={overlayWithDefaults} />
+	{/if}
+	{#if overlayWithDefaults.targetType === OverlayTargetType.Areas}
+		<AreasOverlayRenderer overlay={overlayWithDefaults} />
+	{/if}
+	{#if overlayWithDefaults.targetType === OverlayTargetType.Container}
+		<ContainerOverlayRenderer overlay={overlayWithDefaults} />
+	{/if}
+	{#if overlayWithDefaults.targetType === OverlayTargetType.Custom}
+		<svelte:component this={overlay.component} overlay={overlayWithDefaults} />
+	{/if}
+</div>
