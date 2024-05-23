a988c46d61bf44924f5d981247c79010d95923b1
Grzegorz Palian <grzegorz.palian@sonalake.com>
2024-05-23 23:07:51 +0200

    Added size variants for small icon buttons

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -17,8 +17,11 @@ const buttonVariants = tv({
 		size: {
 			default: 'h-10 px-4 py-2',
 			sm: 'h-9 rounded-md px-3',
+			xs: 'h-6 px-2',
 			lg: 'h-11 rounded-md px-8',
 			icon: 'h-10 w-10',
+			'icon-sm': 'h-8 w-8',
+			'icon-xs': 'h-6 w-6',
 		},
 	},
 	defaultVariants: {
