9dd75fcfbb79e94867c8ddb39a7bc860bb816025
Grzegorz Palian <grzegorz.palian@sonalake.com>
2024-06-01 00:33:02 +0200

    Memoized OverlayRenderer

--- src/lib/components/editable-grid/overlay-renderers/overlay-renderer.component.tsx
+++ src/lib/components/editable-grid/overlay-renderers/overlay-renderer.component.tsx
@@ -5,45 +5,45 @@ import {
 import { CellsOverlayRenderer } from "$/lib/components/editable-grid/overlay-renderers/cells-overlay-renderer.component";
 import { ContainerOverlayRenderer } from "$/lib/components/editable-grid/overlay-renderers/container-overlay-renderer.component";
 import { LinesOverlayRenderer } from "$/lib/components/editable-grid/overlay-renderers/lines-overlay-renderer.component";
-import { useMemo } from "react";
+import { memo, useMemo } from "react";
 
 export interface OverlayRendererProps {
   overlay: EditableGridOverlayData;
 }
 
-export const OverlayRenderer: React.FC<OverlayRendererProps> = ({
-  overlay,
-}) => {
-  const overlayWithDefaults = useMemo(
-    () => ({
-      ...overlay,
-      zIndex: overlay.zIndex ?? 0,
-      pointerEvents: overlay.pointerEvents ?? "auto",
-    }),
-    [overlay],
-  );
+export const OverlayRenderer: React.FC<OverlayRendererProps> = memo(
+  function OverlayRenderer({ overlay }) {
+    const overlayWithDefaults = useMemo(
+      () => ({
+        ...overlay,
+        zIndex: overlay.zIndex ?? 0,
+        pointerEvents: overlay.pointerEvents ?? "auto",
+      }),
+      [overlay],
+    );
 
-  return (
-    <div className="contents">
-      {overlayWithDefaults.targetType === OverlayTargetType.Cells && (
-        <CellsOverlayRenderer overlay={overlayWithDefaults} />
-      )}
-      {overlayWithDefaults.targetType === OverlayTargetType.Lines && (
-        <LinesOverlayRenderer overlay={overlayWithDefaults} />
-      )}
-      {/* TODO */}
-      {overlayWithDefaults.targetType === OverlayTargetType.Areas && null}
-      {/* TODO */}
-      {overlayWithDefaults.targetType === OverlayTargetType.Container && (
-        <ContainerOverlayRenderer overlay={overlayWithDefaults} />
-      )}
-      {overlayWithDefaults.targetType === OverlayTargetType.Custom &&
-        (() => {
-          const Component = overlayWithDefaults.component;
-          if (!Component) return null;
+    return (
+      <div className="contents">
+        {overlayWithDefaults.targetType === OverlayTargetType.Cells && (
+          <CellsOverlayRenderer overlay={overlayWithDefaults} />
+        )}
+        {overlayWithDefaults.targetType === OverlayTargetType.Lines && (
+          <LinesOverlayRenderer overlay={overlayWithDefaults} />
+        )}
+        {/* TODO */}
+        {overlayWithDefaults.targetType === OverlayTargetType.Areas && null}
+        {/* TODO */}
+        {overlayWithDefaults.targetType === OverlayTargetType.Container && (
+          <ContainerOverlayRenderer overlay={overlayWithDefaults} />
+        )}
+        {overlayWithDefaults.targetType === OverlayTargetType.Custom &&
+          (() => {
+            const Component = overlayWithDefaults.component;
+            if (!Component) return null;
 
-          return <Component overlay={overlayWithDefaults} />;
-        })()}
-    </div>
-  );
-};
+            return <Component overlay={overlayWithDefaults} />;
+          })()}
+      </div>
+    );
+  },
+);
